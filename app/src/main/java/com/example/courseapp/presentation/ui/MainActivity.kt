3bb48c3a360f4a72b7903356ef14ac08cc427336
Danila <kurdukovdanila234@gmail.com>
2025-10-21 10:41:13 +0300

    Made packages in modules

--- app/src/main/java/com/example/courseapp/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/courseapp/presentation/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.courseapp
+package com.example.courseapp.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.courseapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
