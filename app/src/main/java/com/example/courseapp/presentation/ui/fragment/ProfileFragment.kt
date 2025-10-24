b56de0a7acdbd554f25c40047c570f6267471cf2
Danila <kurdukovdanila234@gmail.com>
2025-10-24 10:26:37 +0300

    Made Profice screen

--- app/src/main/java/com/example/courseapp/presentation/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/courseapp/presentation/ui/fragment/ProfileFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.courseapp.R
+import com.google.android.material.textview.MaterialTextView
 
 
 class ProfileFragment : Fragment() {
@@ -18,4 +19,23 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+    private lateinit var rowSupport: View
+    private lateinit var rowSettings: View
+    private lateinit var rowLogOut: View
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rowSupport = view.findViewById(R.id.row_support)
+        rowSettings = view.findViewById(R.id.row_settings)
+        rowLogOut = view.findViewById(R.id.row_logOut)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        rowSupport.findViewById<MaterialTextView>(R.id.text_view_actionName).text = "Написать в поддержку"
+        rowSettings.findViewById<MaterialTextView>(R.id.text_view_actionName).text = "Настройки"
+        rowLogOut.findViewById<MaterialTextView>(R.id.text_view_actionName).text = "Выйти из аккаунта"
+    }
 }
